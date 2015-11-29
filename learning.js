a3fb6dc62ad88ba254ff3b1a1928dc694291e1b7
Noah Dealmeida <noahdealmeida@gmail.com>
2015-11-29 12:23:01 -0500

    Adding code

--- learning.js
+++ learning.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+console.log("Hello world!")
+
+//console.log("x:",x.value)
+
+addValues=function(a,b){
+    // parseFload just in case a and b being passed as strings
+    return parseFloat(a)+parseFloat(b)
+}
+
+x.onkeyup=y.onkeyup=function(){
+   z.textContent= addValues(x.value,y.value)
+}
