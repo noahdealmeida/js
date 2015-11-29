2068eb4b303e0c250d9348815e53329303307003
Noah Dealmeida <noahdealmeida@gmail.com>
2015-11-29 17:52:25 -0500

    no message

--- learning.js
+++ learning.js
@@ -4,7 +4,7 @@ console.log("Hello world!")
 
 addValues=function(a,b){
     // parseFload just in case a and b being passed as strings
-    return parseFloat(a)+parseFloat(b)
+    return parseFloat(a)-parseFloat(b)
 }
 
 x.onkeyup=y.onkeyup=function(){
