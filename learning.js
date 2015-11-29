d64bece998d6d2643b620c9f5aec3889c5382ca3
Noah Dealmeida <noahdealmeida@gmail.com>
2015-11-29 18:03:49 -0500

    Changes

--- learning.js
+++ learning.js
@@ -4,9 +4,18 @@ console.log("Hello world!")
 
 addValues=function(a,b){
     // parseFload just in case a and b being passed as strings
+    return parseFloat(a)+parseFloat(b)
+}
+
+subtractValues=function(a,b){
+    // parseFload just in case a and b being passed as strings
     return parseFloat(a)-parseFloat(b)
 }
 
 x.onkeyup=y.onkeyup=function(){
    z.textContent= addValues(x.value,y.value)
+}
+
+x2.onkeyup=y2.onkeyup=function(){
+   z2.textContent= subtractValues(x2.value,y2.value)
 }
